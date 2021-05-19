0f9b80b79bbb963bf3ffc316a890447f562040be
Aparna-A <57554788+Aparna-A@users.noreply.github.com>
2021-05-19 23:37:34 +0530

    Update pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -4,7 +4,7 @@ int main(){
 	int side1 = get_int("Enter side1 value:");
 	int side2 = get_int("Enter side2 value:");
 	int side3 = get_int("Enter side3 value");
-	if(side1*side1 == side2*side2+side3*side3 || side2*side2 = side1*side1+side3*side3 || side3*side3=side1*side1+side2*side2)
+	if(side1*side1 == side2*side2+side3*side3 || side2*side2 == side1*side1+side3*side3 || side3*side3 == side1*side1+side2*side2)
 	{
 		printf("yes");
 	}
