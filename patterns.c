fa649aa8acaa5750e1c96f54cedb4bd6e281ec01
Aparna-A <57554788+Aparna-A@users.noreply.github.com>
2021-05-20 18:23:22 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -9,21 +9,20 @@ int main(void){
 			for(int i=0; i<n; i++){
 				printf("#");
 			}
-				printf("\n");
-				n--;
-			}
-	
-		}
-		else if( x==2){
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<=n-i;j++){
-				printf(" ");
-			}
-			for(int k=1; k<=i; k++){
-				printf("#");
-				
-			}
 			printf("\n");
+			n--;
+		}
+	
+	}
+	else if( x==2){
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n-i;j++){
+			printf(" ");
+		}
+		for(int k=1; k<=i; k++){
+			printf("#");
+		}
+		printf("\n");
 		}
 	}
 	return 0;
